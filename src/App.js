c25ad83d6647fcdbf9e1975b0136da617ef2ffcd
JiHwanLee <kdgosu53@gmail.com>
2022-05-31 20:28:43 +0900

    make video item

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       redirect: 'follow'
     };
     
-    fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyA-GeAio_uars1qVGYqNusRdorj8y4CCYw&chart=mostPopular&maxResults=2&part=snippet&regionCode=kr", requestOptions)
+    fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyA-GeAio_uars1qVGYqNusRdorj8y4CCYw&chart=mostPopular&maxResults=25&part=snippet&regionCode=kr", requestOptions)
       .then(response => response.json())
       .then(result => {
         console.log(result)
