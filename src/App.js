aac59b2256a5cabe0cfccf3f5321a837eb7dd415
JiHwanLee <kdgosu53@gmail.com>
2022-06-04 16:11:00 +0900

    make search feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    console.log(videos);
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyA-GeAio_uars1qVGYqNusRdorj8y4CCYw&part=snippet&type=video&maxResults=25&q=${query} `, requestOptions)
+      .then(response => response.json())
+      .then(result => (
+        result.items.map(item => 
+          ({...item, id: item.id.videoId}))
+        // setVideos(aa)
+      ))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  };
 
   useEffect(() => {
     console.log(`useEffect`);
@@ -16,15 +34,18 @@ function App() {
     fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyA-GeAio_uars1qVGYqNusRdorj8y4CCYw&chart=mostPopular&maxResults=25&part=snippet&regionCode=kr", requestOptions)
       .then(response => response.json())
       .then(result => {
-        console.log(result)
         setVideos(result.items)
       })
       .catch(error => console.log('error', error));
 
 
   }, []);
-
-  return <VideoList videos={videos}/> ;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search}/>
+      <VideoList videos={videos}/> ;
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react';
+import styles from './search_header.module.css';
+
+const SearchHeader = ({onSearch}) => {
+    const inputRef = useRef();
+
+    const hadnleSearch = () => {
+        const value = inputRef.current.value;
+        console.log(value);
+        onSearch(value);
+    };
+    const onClick = (e) => {
+        console.log(`onClick`);
+        hadnleSearch();
+    };
+
+    const onKeyDown = (e) => {
+        console.log(`onKeyDown`);
+        // console.log(e);
+        if(e.key === 'Enter') {
+            hadnleSearch();
+        }
+    };
+
+    
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img className={styles.img} src="/images/yt_logo.ico" alt="" />
+                <h1 className={styles.title}> Youtube </h1>
+            </div>
+            
+
+            <input className={styles.input} ref={inputRef} type="text" placeholder='Search...' onKeyDown={onKeyDown} />
+            <button className={styles.button} type='submit' onClick={onClick}>
+                <img className={styles.buttonImg} src="/images/search.png" alt="" />
+            </button>
+        </header>
+    )
+}
+
+export default SearchHeader;
